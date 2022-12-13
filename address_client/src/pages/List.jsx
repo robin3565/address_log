32e94769a343cbb259894cf8ba9bf9cea53eaf21
robin <juzi30553565@gmail.com>
2022-12-13 17:35:01 +0900

    FIX: SCAN

--- address_client/src/pages/List.jsx
+++ address_client/src/pages/List.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { Link, useLocation, useNavigate } from "react-router-dom";
-import { options } from "../utils/constants";
+import { OPTIONS, MAIN_NET_OPTIONS } from "../utils/constants";
 
 const List = () => {
   const location = useLocation();
@@ -29,6 +29,8 @@ const List = () => {
     }
   }, []);
 
+  console.log(res);
+
   return (
     <div className="list">
       <div className="layoutfix">
@@ -46,56 +48,78 @@ const List = () => {
         <div className="inputBox">
           <p>{address}</p>
         </div>
-        <p>
-          총 <em>{res?.total_cnt}번</em>의 정보 등록 내역이 있습니다.
-        </p>
-        <div className="btnBox">
-          <span>
-            <em>안전해요</em>
-            <em>{res?.safe_cnt}</em>
-          </span>
-          <span className="danger">
-            <em>위험해요</em>
-            <em>{res?.danger_cnt}</em>
-          </span>
-        </div>
+        {res?.data?.length > 0 ? (
+          <p>
+            총 <em>{res?.total_cnt}번</em>의 정보 등록 내역이 있습니다.
+          </p>
+        ) : (
+          <p>
+            <em>등록된 내역이 없습니다.</em>
+          </p>
+        )}
+
+        {res?.data?.length > 0 && (
+          <div className="btnBox">
+            <span>
+              <em>안전해요</em>
+              <em>{res?.safe_cnt}</em>
+            </span>
+            <span className="danger">
+              <em>위험해요</em>
+              <em>{res?.danger_cnt}</em>
+            </span>
+          </div>
+        )}
         <div className="bottomLink">
           <em>해당 지갑주소 정보 등록을 하고 싶어요!</em>
           <Link to="/notify">정보 등록 바로가기</Link>
         </div>
-        <table className="tg">
-          <thead>
-            <tr>
-              <th className="tg-nrix">번호</th>
-              <th className="tg-nrix">종류</th>
-              <th className="tg-nrix">피해 분류</th>
-              <th className="tg-nrix">등록일</th>
-              <th class="tg-nrix">스캔</th>
-            </tr>
-          </thead>
-          <tbody>
-            {res?.data?.map((item, index) => {
-              return (
-                parseInt(item.options) > 0 && (
-                  <tr key={index}>
-                    <td className="tg-nrix">{index + 1}</td>
-                    <td className="tg-nrix">{item.main_net}</td>
-                    <td className="tg-cly1">
-                      {parseInt(item.options) == 5
-                        ? item.option_contents
+        {res?.data?.length > 0 && (
+          <table className="tg">
+            <thead>
+              <tr>
+                <th className="tg-nrix">번호</th>
+                <th className="tg-nrix">종류</th>
+                <th className="tg-nrix">피해 분류</th>
+                <th className="tg-nrix">등록일</th>
+                <th class="tg-nrix">스캔</th>
+              </tr>
+            </thead>
+            <tbody>
+              {res?.data?.map((item, index) => {
+                return (
+                  parseInt(item.options) > 0 && (
+                    <tr key={index}>
+                      <td className="tg-nrix">{index + 1}</td>
+                      <td className="tg-nrix">
+                        {MAIN_NET_OPTIONS[parseInt(item.main_net)]?.main_net}
+                      </td>
+                      <td className="tg-cly1">
+                        {parseInt(item.options) == 5
                           ? item.option_contents
-                          : "기타"
-                        : options[parseInt(item.options)]?.text}
-                    </td>
+                            ? item.option_contents
+                            : "기타"
+                          : OPTIONS[parseInt(item.options)]?.text}
+                      </td>
 
-                    <td className="tg-cly1">{item.reg_date.split("T")[0]}</td>
-                    <td class="tg-nrix"><a href="">보기</a></td>
-                  </tr>
-                )
-              );
-            })}
-          </tbody>
-        </table>
+                      <td className="tg-cly1">{item.reg_date.split("T")[0]}</td>
+                      <td class="tg-nrix">
+                        <a
+                          target="_blank"
+                          href={`${
+                            MAIN_NET_OPTIONS[parseInt(item.main_net)]?.url
+                          }${address}`}
+                        >
+                          보기
+                        </a>
+                      </td>
+                    </tr>
+                  )
+                );
+              })}
+            </tbody>
+          </table>
+        )}
       </div>
     </div>
   );
