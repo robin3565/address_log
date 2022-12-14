86bb2e8e3ca96c3b7687f95180e480994e72150b
Your Name <you@example.com>
2022-12-14 15:26:00 +0900

    ADD : RESPONSIVE

--- address_client/src/pages/Home.jsx
+++ address_client/src/pages/Home.jsx
@@ -21,10 +21,12 @@ const Home = () => {
       <div className="mainTitle">
         <h5>보다 안전한 암호화폐 거래</h5>
         <p>암호화폐 지갑주소 검색</p>
-        <input type="text" name="" id="" placeholder="검색할 계좌 주소를 입력해 주세요." onChange={(e) => setAddress(e.target.value)}/>
-        <button onClick={handleSearch}>
-          <img src="img/searchIcon.png" alt="" />
-        </button>
+        <div className="inputBox">
+          <input type="text" name="" id="" placeholder="검색할 계좌 주소를 입력해 주세요." onChange={(e) => setAddress(e.target.value)}/>
+          <button onClick={handleSearch}>
+            <img src="img/searchIcon.png" alt="" />
+          </button>
+        </div>
         <div className="bottomLink">
           <em>지갑주소 정보 등록을 하고 싶어요!</em>
           <Link to="/notify">정보 등록 바로가기</Link>
